15af491112e3ce4721af909cf89392260db6c251
subhash <subhashds025@gmail.com>
2024-11-27 12:36:54 +0530

    Defender

---
+++ src/main/java/com/Defender/DefendApplication.java
@@ -0,0 +1,13 @@
+package com.Defender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DefendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DefendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Defender/DefendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Defender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DefendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
